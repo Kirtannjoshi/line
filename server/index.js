b2b80cc353a481e9cccd15585d73e81023b935d5
Kirtannjoshi <kirtanjoshi2004@gmail.com>
2025-07-15 21:03:47 +0530

    Implement user authentication backend with Mongoose, bcrypt, JWT, and routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import userRoutes from './routes/userRoutes.js';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
 
+import connectDB from './db.js';
+
 const app = express();
+
+app.use(express.json());
+
+connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -13,6 +20,9 @@ const io = new Server(server, {
 });
 
 app.use(cors());
+
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => res.send('API Running'));
 
 server.listen(5000, () => console.log('Server running on port 5000'));
